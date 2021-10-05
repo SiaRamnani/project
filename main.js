bd49527d9cd56bd6a5a1bb9c2759f455a667d211
SiaRamnani <85045958+SiaRamnani@users.noreply.github.com>
2021-10-05 09:05:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 }
 console.log("ml5 version",ml5.version);
 
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/0N09Fn7vp/model.json",modelloaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/tL_xQmRmO/",modelloaded);
 
 function modelloaded()
 {
